83f9ddd68523ea1940da37323f2addd66cff83b5
Ailsa Don <ailsazawadi415@gmail.com>
2025-12-18 16:13:56 +0300

    Updated url in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
     const [message, setMessage] = useState("");
     const [success, setSuccess] = useState(false);
 
-    const BACKEND_URL = "https://api.my-duka.co.ke/";
+    const BACKEND_URL = "https://api.my-duka.co.ke";
 
     // Redirect if already logged in
     useEffect(() => {
